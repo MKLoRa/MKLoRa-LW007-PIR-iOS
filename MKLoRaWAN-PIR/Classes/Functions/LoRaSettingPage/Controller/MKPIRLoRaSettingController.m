2dbadf73f511f843ab3f72e45dd8cdbd7703ae55
lovexiaoxia <aadyx2007@163.com>
2022-09-29 14:35:46 +0800

    完善功能

--- MKLoRaWAN-PIR/Classes/Functions/LoRaSettingPage/Controller/MKPIRLoRaSettingController.m
+++ MKLoRaWAN-PIR/Classes/Functions/LoRaSettingPage/Controller/MKPIRLoRaSettingController.m
@@ -433,6 +433,13 @@ MKLoRaSettingCHCellDelegate>
         MKLoRaSettingCHCellModel *cellModel = self.optionsList6[0];
         cellModel.chHighIndex = chHighIndex;
         cellModel.chLowValueList = [self.dataModel DRLValueList];
+        
+        if (self.dataModel.DRH < self.dataModel.DRL) {
+            cellModel.chLowIndex = cellModel.chLowValueList.count - 1;
+            self.dataModel.DRL = self.dataModel.DRH;
+        }
+        
+        [self.tableView mk_reloadSection:9 withRowAnimation:UITableViewRowAnimationNone];
         return;
     }
 }
@@ -458,6 +465,14 @@ MKLoRaSettingCHCellDelegate>
         MKLoRaSettingCHCellModel *cellModel = self.optionsList6[0];
         cellModel.chLowIndex = chLowIndex;
         cellModel.chHighValueList = [self.dataModel DRHValueList];
+        
+        if (self.dataModel.DRH < self.dataModel.DRL) {
+            cellModel.chHighIndex = 0;
+            self.dataModel.DRH = self.dataModel.DRL;
+        }
+        
+        [self.tableView mk_reloadSection:9 withRowAnimation:UITableViewRowAnimationNone];
+        
         return;
     }
 }

5a0b0b94027619fbe2acd37f1ecba5f1f29888c1
lovexiaoxia <aadyx2007@163.com>
2022-12-08 14:42:33 +0800

    完善功能

--- MKLoRaWAN-PIR/Classes/Functions/TabBarPage/Controller/MKPIRTabBarController.m
+++ MKLoRaWAN-PIR/Classes/Functions/TabBarPage/Controller/MKPIRTabBarController.m
@@ -130,6 +130,9 @@
         [self showAlertWithMsg:@"Factory reset successfully!Please reconnect the device." title:@"Factory Reset"];
         return;
     }
+    //异常断开
+    NSString *msg = [NSString stringWithFormat:@"Device disconnected for unknown reason.(%@)",type];
+    [self showAlertWithMsg:msg title:@"Dismiss"];
 }
 
 - (void)centralManagerStateChanged{
